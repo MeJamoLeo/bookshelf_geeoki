e1427337c27f7d56cf253b101c60a5be2189014a
MeJamoLeo <reonarudo11919273@gmail.com>
2020-02-24 19:48:27 +0900

    sinatraとpostgresqlを繋げる手前

--- main.rb
+++ main.rb
@@ -1,11 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
+require 'pry'
+require 'pg'
+
+
+client = PG::connect(
+  :host => "localhost",
+  :user => 'leo',
+  :password => '',
+  :dbname => "library_geek")
 
 # トップページ
-get '/' do
-  return erb :index
+get '/login' do
+  @current_directry = Dir.pwd.split('/').last;
+  return erb :login
 end
+
+post '/login' do
+  @email = params[:email]
+  @password = params[:password]
+
+  if
+  redirect '/books'
+end
+
 
 get '/books' do
   return erb :books
@@ -17,4 +36,27 @@ end
 
 get '/mypage' do
   return erb :mypage
+end
+
+
+get '/users' do
+  sql = "select * from users;"
+  @users = db.exec_params(sql).to_a
+  # @users の戻り値のサンプル
+  # [
+  #   {
+  #     "id" => "1",
+  #     "name" => "kinjo",
+  #     "email" => "kinjo@mail.com",
+  #     "password" => "kinjo"
+  #   },
+  #   {
+  #     "id" => "2",
+  #     "name" => "higa",
+  #     "email" => "higa@mail.com",
+  #     "password" => "higa"
+  #   }
+  # ]
+
+  erb :users
 end
--- sample.sql
+++ sample.sql
@@ -1,16 +1,2 @@
-
-CREATE TABLE users
-(
-  id SERIAL NOT NULL PRIMARY KEY ,
-  name VARCHAR( 25 ) NOT NULL ,
-  email VARCHAR( 35 ) NOT NULL ,
-  password VARCHAR( 60 ) NOT NULL ,
-  UNIQUE (email)
-);
-
-INSERT INTO users
-  (name, email, password)
-VALUES
-  ('kinjo', 'kinjo@mail.com', 'kinjo')
-;
-3
+SELECT *
+FROM users;
