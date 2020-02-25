5d4fb5e13f178f3af2c9c49c16fde50f1a1e4d92
MeJamoLeo <reonarudo11919273@gmail.com>
2020-02-25 18:23:40 +0900

    head,header,footer をテンプレファイルに分けた．none.erbを作成した．

--- main.rb
+++ main.rb
@@ -1,24 +1,57 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/cookies'
 require 'active_record'
 require 'pry'
 require 'pg'
+require 'erb'
 
-def db
-client = PG::connect(
-  :host => "localhost",
-  :user => 'leo',
-  :password => '',
-  :dbname => "library_geek")
+enable :sessions
+
+
+ActiveRecord::Base.configurations = YAML.load_file('database.yml')
+ActiveRecord::Base.establish_connection(:development)
+
+class User < ActiveRecord::Base
+end
+
+class Book < ActiveRecord::Base
+end
+
+class History < ActiveRecord::Base
+end
+
+class Tag < ActiveRecord::Base
+end
+
+class Tagmap < ActiveRecord::Base
+end
+class Bookownermap < ActiveRecord::Base
+end
+class Authoermaps < ActiveRecord::Base
 end
 
 # トップページ
 get '/login' do
-  @current_directry = Dir.pwd.split('/').last;
-  return erb :login
+  return erb :login, layout: :none
 end
 
+post '/login' do
 
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  @email = params[:email]
+  @password = params[:password]
+  @user_name = params[:name]
+
+  User.create(:email=> @email, :password=> @password, :name=> @user_name)
+  redirect '/login'
+end
 
 
 get '/books' do
@@ -35,8 +68,7 @@ end
 
 
 get '/users' do
-  sql = "select * from users;"
-  @users = db.exec_params(sql).to_a
+  @users = User.all
   erb :users
 end
 
@@ -57,4 +89,26 @@ get '/users/:id' do
   # }
 
   erb :users
+end
+
+# ----------------------------------------------------
+
+
+post '/books/new' do
+  @title = params[:title]
+  @description = params[:description]
+  @thumbnail = params[:thumbnail]
+
+  @authoer = params[:authoer]
+  @tag = params[:tag]
+
+  Book.create(:title=> @title, :description=> @description, :thumbnail=> @thumbnail)
+  # セッションで投稿したユーザーのidをゲットしたい
+  @user_id = 1
+  Tag.create(:tag_name=> @tag, )
+  Authoermap.create()
+end
+
+get '/books/new' do
+  erb :add_book
 end
