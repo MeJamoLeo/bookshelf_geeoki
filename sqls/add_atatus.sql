f50729a30e38b3a4c59679838aecf48b44d4e96a
MeJamoLeo <reonarudo11919273@gmail.com>
2020-03-02 01:24:49 +0900

    データを入れた

---
+++ sqls/add_atatus.sql
@@ -0,0 +1,32 @@
+INSERT INTO statuses
+VALUES
+  (
+    0,
+    'リクエスト承認待ち'
+);
+
+
+INSERT INTO statuses
+VALUES
+  (
+    1,
+    'リクエスト承認済み'
+);
+
+
+INSERT INTO statuses
+VALUES
+  (
+    2,
+    '返却確認待ち'
+);
+
+
+INSERT INTO statuses
+VALUES
+  (
+    3,
+    '返却確認済み'
+);
+
+
